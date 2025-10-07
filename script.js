0c5a09314109afcece98a6d8d952a2ccb5c35401
jhon-kevin2005 <jhonkevinhuataylucano@gmail.com>
2025-10-07 12:31:03 -0500

    script.js
    Signed-off-by: jhon-kevin2005 <jhonkevinhuataylucano@gmail.com>

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const cursorLight = document.getElementById("cursor-light");
 document.addEventListener("mousemove", (e) => {
   cursorLight.style.background = `
     radial-gradient(circle at ${e.clientX}px ${e.clientY}px,
-    rgba(255,127,42,0.2), transparent 50%)`;
+    rgba(255,127,42,0.2), transparent 50%)
+  `;
 });
 
 // Menú móvil
-const menuToggle = document.querySelector(".menu-toggle");
-const navUl = document.querySelector("header nav ul");
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
-  navUl.classList.toggle("show");
+  navLinks.classList.toggle("active");
 });
